d14fdc90cf700145fd9db6e9e793aa7c49d5a8e2
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2024-09-23 11:32:03 +0300

    CORS politics

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+app.use(cors());
+
+// app.use((req,res,next) =>{
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Methods', '*');  // instal cors!
+    // res.setHeader('Access-Control-Allow-Headers', '*');
+
+    // next();
+// })
 
 app.get('/', (req, res) => {
     res.send('RESTful service');
